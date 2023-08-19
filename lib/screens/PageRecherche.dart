cdcb322c6365db812f1d7295d21777898732721f
AzharTCHATCHIBARA <tozhar22@gmail.com>
2023-08-19 21:09:12 +0000

    icone circle

--- lib/screens/PageRecherche.dart
+++ lib/screens/PageRecherche.dart
@@ -61,9 +61,6 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Recherche d\'événements'),
-      ),
       body: Column(
         children: [
           Padding(
